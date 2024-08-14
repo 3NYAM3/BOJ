38fc74386c4ab744fbdf50c4bf4c63089665e765
Doyeong Lee <09jjrr21@naver.com>
2024-08-14 22:03:47 +0900

    ✨feat: 1439
    - 2차원 배열 끝부터 뒤집으면서 최소 회수를 생각하면 됨

---
+++ 그리디/BOJ1455.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1455 {
+    static int row, col;
+    static int[][] coin;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+
+        coin = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < col; j++) {
+                coin[i][j] = Integer.parseInt(str.charAt(j) + "");
+            }
+        }
+
+        int count = 0;
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
+                if (coin[i][j] == 1) {
+                    for (int k = i; k >= 0; k--) {
+                        for (int l = j; l >= 0; l--) {
+                            coin[k][l] = coin[k][l] == 1 ? 0 : 1;
+                        }
+                    }
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
