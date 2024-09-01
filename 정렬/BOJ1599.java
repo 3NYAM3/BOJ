ae90bf02272ca4971a730c188a157b6e43bde380
Doyeong Lee <09jjrr21@naver.com>
2024-09-01 22:25:52 +0900

    ✨feat: 1599
    - 새로만든 언어는 영어와 같은 오름차순으로 c 대신 k 를 넣고 n r과 o 사이에 ng라는 알파벳을 넣었다.
    - 입력받은 언어에서 replace를 통해 k를 c로 바꾸고 ng를 n}로 바꾸서 정렬 후 원상복귀 하면 된다.
    - ng 를 n}으로 치환시킨 이유는 }가 아스키코드에서 z다음으로 오는 문자이기 때문

---
+++ 정렬/BOJ1599.java
@@ -0,0 +1,35 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ1599 {
+    static int n;
+    static String[] words;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        words = new String[n];
+        for (int i = 0; i < n; i++) {
+            words[i] = br.readLine();
+            words[i] = words[i].replace('k', 'c');
+            words[i] = words[i].replace("ng", "nz");
+        }
+
+        Arrays.sort(words);
+
+        for (int i = 0; i < n; i++) {
+            words[i] = words[i].replace("nz", "ng");
+            words[i] = words[i].replace('c', 'k');
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(words[i]);
+        }
+
+    }
+}
