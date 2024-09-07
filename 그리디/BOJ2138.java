1c9fab64fb5df9fc7aeb29315477f6cf71cf4ded
Doyeong Lee <09jjrr21@naver.com>
2024-09-07 22:02:41 +0900

    ✨feat: 2195
    - 그리디 알고리즘을 이용하여 첫 번째 전구를 키거나 키지 않거나 이 두가지의 경우의 수를 모두 생각하여 최소 횟수를 구한다.
    - 목표 상태와 계속 비교해가면서 전구를 킬건지 결정해야함
    - 첫번째 전구는 두번째 전구를 킬때 최종적으로 결정되므로 첫번째 전구가 목표 상태와 같게 두번째를 조작해야한다.
    - 이런식으로 끝까지 전구를 조작하여 첫 번째 전구를 켰을때와 안켰을 때 의 횟수를 비교하여 최소값 구하면 됨

---
+++ 그리디/BOJ2138.java
@@ -0,0 +1,72 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2138 {
+    static int n;
+    static int[] before, after;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        before = new int[n];
+        after = new int[n];
+
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        for (int i = 0; i < n; i++) {
+            before[i] = Integer.parseInt(s1.charAt(i) + "");
+            after[i] = Integer.parseInt(s2.charAt(i) + "");
+        }
+
+        int press = solution(false);
+        int notPress = solution(true);
+
+        if (press == -1 && notPress == -1) {
+            System.out.println(-1);
+        } else if (press == -1) {
+            System.out.println(notPress);
+        } else if (notPress == -1) {
+            System.out.println(press);
+        } else {
+            System.out.println(Math.min(press, notPress));
+        }
+
+
+    }
+
+    static int solution(boolean press) {
+        int[] bulbs = before.clone();
+        int count = 0;
+
+        if (press) {
+            press(bulbs, 0);
+            count++;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (bulbs[i - 1] != after[i - 1]) {
+                press(bulbs, i);
+                count++;
+            }
+        }
+
+        if (bulbs[n - 1] != after[n - 1]) {
+            return -1;
+        }
+        return count;
+    }
+
+    static void press(int[] bulbs, int index) {
+        bulbs[index] = 1 - bulbs[index];
+        if (index - 1 >= 0) {
+            bulbs[index - 1] = 1 - bulbs[index - 1];
+        }
+        if (index + 1 < n) {
+            bulbs[index + 1] = 1 - bulbs[index + 1];
+        }
+    }
+}
