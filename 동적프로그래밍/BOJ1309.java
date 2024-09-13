f09313cb1120ca589106d73fd680ffa80187de8e
Doyeong Lee <09jjrr21@naver.com>
2024-09-13 21:17:31 +0900

    ✨feat: 1309
    - 11057번 문제와 유사한 문제
    - dp[i][j] = n : i번째 줄에서, j번째 칸에 동물을 놓을 수 있는 경우의 수는 n, j=0일때는 없는 경우
    - 이번 방에 놓지 않는 경우: 이전방은 j=0,1,2
    - 이번 방에 j = 1일 경우: 이전방은 j=0,2
    - 이번 방엔 j = 2일 경우: 이전방은 j=0,1

---
+++ 동적프로그래밍/BOJ1309.java
@@ -0,0 +1,30 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1309 {
+    static int n;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        dp = new int[n + 1][3];
+
+        dp[1][0] = dp[1][1] = dp[1][2] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901;
+        }
+
+        int answer = (dp[n][0] + dp[n][1] + dp[n][2]) % 9901;
+        System.out.println(answer);
+        br.close();
+    }
+
+}
